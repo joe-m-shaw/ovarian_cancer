2e341e4730315eb96b3d2103c721103c7362b48e
Joe Shaw <57156322+joeshaw824@users.noreply.github.com>
2025-07-24 09:43:08 +0100

    Add tests and messages

--- scripts/glvar_tvar_dnadb_extract.R
+++ scripts/glvar_tvar_dnadb_extract.R
@@ -10,18 +10,27 @@ gi_csv_cleaned <- read_csv(file = paste0(config::get(
     nhsno = col_character()
   ))
 
+stopifnot(nrow(gi_csv_cleaned) != 0)
+stopifnot(anyNA(gi_csv_cleaned$nhsno) == FALSE)
+
 gi_nhsnos <- gi_csv_cleaned$nhsno
 
 # Identify all lab numbers from patients ----------------------------------
+
+message("Finding all lab numbers for patients with GI results")
 
 labno_df <- sample_tbl |> 
   filter(nhsno %in% gi_nhsnos) |> 
   select(labno, nhsno) |> 
   collect()
 
+stopifnot(length(setdiff(unique(labno_df$nhsno), gi_nhsnos)) == 0)
+
 labno_query <- unique(labno_df$labno)
 
 # Extract all results for lab numbers -------------------------------------
+
+message("Extracting DNA Database results for all lab numbers")
 
 dnadb_results <- results_tbl |> 
   filter(labno %in% labno_query) |> 
@@ -29,28 +38,49 @@ dnadb_results <- results_tbl |>
          genocomm) |> 
   collect()
 
+stopifnot(nrow(dnadb_results) != 0)
 
 # Extract germline variant results ----------------------------------------
 
+message("Finding germline DNA Database results")
+
+icp_test_strings <- unique(grep(pattern = "hs2(\\s|)icp", 
+            x = dnadb_results$test, 
+            ignore.case = TRUE, 
+            value = TRUE))
+
 glvar_dnadb_results <- dnadb_results |> 
-  filter(test %in% unique(grep(pattern = "hs2(\\s|)icp", 
-                               x = dnadb_results$test, 
-                               ignore.case = TRUE, 
-                               value = TRUE))) |> 
+  filter(test %in% c(icp_test_strings,
+                     "NGS SSXT ICP",
+                     "ICP PANEL",
+                     "SSXT ICP NGS",
+                     "ICP SSXT NGS",
+                     "Panel re-analysis of 24043064 from WS144546",
+                     "NGS SSXT ICPv4",
+                     "SSXTHS2 ICPv4",
+                     "ICPv4 NGS SSXT HS2")) |> 
   left_join(labno_df, by = "labno") |> 
   relocate(nhsno)
 
 # Extract tumour variant results ------------------------------------------
 
+message("Finding tumour variant DNA Database results")
+
+pansolid_test_strings <- unique(grep(pattern = "seq\\span", 
+                                     x = dnadb_results$test, 
+                                     ignore.case = TRUE, 
+                                     value = TRUE))
+
 tvar_dnadb_results <- dnadb_results |> 
-  filter(test %in% unique(grep(pattern = "seq\\span", 
-                               x = dnadb_results$test, 
-                               ignore.case = TRUE, 
-                               value = TRUE))) |> 
+  filter(test %in% c(pansolid_test_strings,
+                     "Proxy reanalysis proxy WS for original WS137613",
+                     "NGS Pansolid")) |> 
   left_join(labno_df, by = "labno") |> 
   relocate(nhsno)
 
 # Export data -------------------------------------------------------------
+
+message("Exporting DNA database tvar and glvar data")
 
 write_csv(glvar_dnadb_results,
           paste0(config::get(
