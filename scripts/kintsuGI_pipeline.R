efb5a93bf79bd4df920d146524bc47698b9340f6
Joe Shaw <57156322+joeshaw824@users.noreply.github.com>
2025-07-24 13:45:51 +0100

    Add formatting to final report

--- scripts/kintsuGI_pipeline.R
+++ scripts/kintsuGI_pipeline.R
@@ -1,21 +1,19 @@
 # kintsuGI pipeline
 
-library(here)
-
 message("Running kintsuGI pipeline")
 
-source(here("scripts/gi_csv_collate.R"))
+source(here::here("scripts/gi_csv_collate.R"))
 
-source(here("scripts/gi_csv_annotate.R"))
+source(here::here("scripts/gi_csv_annotate.R"))
       
-source(here("scripts/glvar_tvar_dnadb_extract.R"))
+source(here::here("scripts/glvar_tvar_dnadb_extract.R"))
 
-source(here("scripts/glvar_tvar_igene_extract.R"))
+source(here::here("scripts/glvar_tvar_igene_extract.R"))
 
-source(here("scripts/glvar_tvar_dnadb_clean.R"))
+source(here::here("scripts/glvar_tvar_dnadb_clean.R"))
 
-source(here("scripts/glvar_tvar_dnadb_igene_bind.R"))
+source(here::here("scripts/glvar_tvar_dnadb_igene_bind.R"))
 
-source(here("scripts/gi_glvar_tvar_join.R"))
+source(here::here("scripts/gi_glvar_tvar_join.R"))
 
 message("kintsuGI pipeline finished")
