9023f517cb5d7dce074dde713797b65aa9afdeaa
Joe Shaw <57156322+joeshaw824@users.noreply.github.com>
2025-07-25 08:46:09 +0100

    Clear environment after each script source

--- scripts/kintsuGI_pipeline.R
+++ scripts/kintsuGI_pipeline.R
@@ -2,17 +2,29 @@
 
 message("Running kintsuGI pipeline")
 
-source(here::here("scripts/gi_csv_collate.R"))
+# source(here::here("scripts/gi_csv_collate.R"))
+
+# rm(list=ls())
 
 source(here::here("scripts/gi_csv_annotate.R"))
+
+rm(list=ls())
       
 source(here::here("scripts/glvar_tvar_dnadb_extract.R"))
 
+rm(list=ls())
+
 source(here::here("scripts/glvar_tvar_igene_extract.R"))
+
+rm(list=ls())
 
 source(here::here("scripts/glvar_tvar_dnadb_clean.R"))
 
+rm(list=ls())
+
 source(here::here("scripts/glvar_tvar_dnadb_igene_bind.R"))
+
+rm(list=ls())
 
 source(here::here("scripts/gi_glvar_tvar_join.R"))
 
