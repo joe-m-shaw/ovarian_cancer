c2c0722cd7a00290310ca6ba6f03e2d0b581c8aa
Joe Shaw <57156322+joeshaw824@users.noreply.github.com>
2025-07-29 09:21:29 +0100

    Update igene extraction to include new panels
    Add steps for inclusion of v2.a and v2.b tBRCA panels in iGene extraction. This required changing the widen_by_test function to use a vector of panel names as input, and to take names from the box rather than field column.

--- scripts/glvar_tvar_igene_extract.R
+++ scripts/glvar_tvar_igene_extract.R
@@ -7,30 +7,33 @@ source(here::here("scripts/connect_to_sql_server.R"))
 
 test_names <- c("PANEL: M2.5 - SeqOne HRD Status",
                 "PANEL: R207.1 - Inherited ovarian cancer (without breast cancer) v4.0 (ICP)",
-                "PANEL: M2_tBRCA_PS")
+                "PANEL: M2_tBRCA_PS",
+                "PANEL: M2_tBRCA_PS v2.a",
+                "PANEL: M2_tBRCA_PS v2.b")
 
 stopifnot(nrow(eval_hrd) != 0)
 stopifnot(length(setdiff(unique(eval_hrd$test_name), test_names)) == 0)
-stopifnot(ncol(eval_hrd) == 7)
+stopifnot(ncol(eval_hrd) == 8)
 
 # Function ----------------------------------------------------------------
 
 message("Extracting glvar and tvar results from iGene")
 
 widen_by_test <- function(df = eval_hrd, 
-                          test_string,
+                          test_vector,
                           prefix_string) {
   
   output <- eval_hrd |> 
-    filter(test_name == test_string) |> 
-    select(-nhsn_umber) |> 
+    filter(test_name %in% test_vector) |> 
     pivot_wider(id_cols = c(referral_number, 
+                            nhsn_umber,
                             test_order_date, 
                             test_identifier),
-                names_from = field,
+                names_from = box,
                 values_from = data_value,
                 names_prefix = prefix_string) |> 
-    clean_names()
+    clean_names() |> 
+    rename(nhsno = nhsn_umber)
   
   if(nrow(output) == 0){
     stop("Output dataframe is empty")
@@ -43,15 +46,19 @@ widen_by_test <- function(df = eval_hrd,
 # Extract tumour and germline variant results -----------------------------
 
 glvar_igene <- widen_by_test(df = eval_hrd,
-                              test_string = "PANEL: R207.1 - Inherited ovarian cancer (without breast cancer) v4.0 (ICP)", 
+                              test_vector = c("PANEL: R207.1 - Inherited ovarian cancer (without breast cancer) v4.0 (ICP)"), 
                               prefix_string = "glvar")
 
+stopifnot(ncol(glvar_igene) == 23)
+
 tvar_igene <- widen_by_test(df = eval_hrd,
-                             test_string = "PANEL: M2_tBRCA_PS", 
-                             prefix_string = "tvar") 
+                             test_vector = c("PANEL: M2_tBRCA_PS",
+                                             "PANEL: M2_tBRCA_PS v2.a",
+                                             "PANEL: M2_tBRCA_PS v2.b"), 
+                             prefix_string = "tvar")
 
 gi_igene <- widen_by_test(df = eval_hrd,
-                          test_string = "PANEL: M2.5 - SeqOne HRD Status", 
+                          test_vector = c("PANEL: M2.5 - SeqOne HRD Status"), 
                           prefix_string = "gi")
 
 # Perform checks ----------------------------------------------------------
@@ -70,6 +77,12 @@ tests_in_wide_tables <- c(unique(glvar_igene$test_identifier),
 
 # Check no tests have been lost from the dataset
 stopifnot(length(setdiff(tests_in_eval_hrd, tests_in_wide_tables)) == 0)
+
+message(paste0("There are ", 
+               length(tests_in_eval_hrd), 
+               " tests in eval_hrd and ",
+               length(tests_in_wide_tables), 
+               " tests in the reformatted data."))
 
 # Remove entries without headline results ---------------------------------
 
