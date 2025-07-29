2e0bc76b8387ac0835702a82adde1e39dc294eef
Joe Shaw <57156322+joeshaw824@users.noreply.github.com>
2025-07-29 09:23:48 +0100

    Update names of columns
    This is required to account for the new structure of the extracted iGene data.
    "glvar_hgvs_description" becomes "glvar_seqv1_descrption".
    "glvar_classification" becomes "glvar_seqv1_classification" etc

--- scripts/glvar_tvar_dnadb_clean.R
+++ scripts/glvar_tvar_dnadb_clean.R
@@ -37,33 +37,37 @@ glvar_dnadb_classifications <- glvar_dnadb_extracted |>
       glvar_snv_result == "BRCA1 Exon 3 3 Copies" ~"No pathogenic variant identified",
       TRUE ~glvar_snv_result
     ),
-    # Separate HGVS nomenclature into glvar_hgvs_description column
-    glvar_hgvs_description = str_extract(string = glvar_snv_result,
+    # Separate HGVS nomenclature into glvar_seqv1_description column
+    glvar_seqv1_description = str_extract(string = glvar_snv_result,
                                          pattern = "(.*)\\s\\d{1,3}%",
                                          group = 1),
     # Remove non-pathogenic BRCA1 variant from sample 24030433 which also
     # has a pathogenic BRCA2 variant
-    glvar_hgvs_description = case_when(
+    glvar_seqv1_description = case_when(
       genotype == "BRCA2 c.4276dup p.(Thr1426AsnfsTer12) 48%; BRCA1 c.5074+13C>A 43%; CNV Analysis Failed" ~"BRCA2 c.4276dup p.(Thr1426AsnfsTer12)",
-      TRUE ~glvar_hgvs_description
+      TRUE ~glvar_seqv1_description
     ),
-    # glvar_description is used in the iGene results to describe CNVs
-    glvar_description = str_extract(string = glvar_cnv_result,
+    # glvar_icnv1_description is used in the iGene results to describe CNVs
+    glvar_icnv1_description = str_extract(string = glvar_cnv_result,
                                   pattern = ".*Ex.*")
     ) |> 
   left_join(glvar_classifications,
-             by = "glvar_hgvs_description") |> 
-  mutate(glvar_classification = case_when(
-    glvar_description == " BRCA1 Ex02 deletion" ~"Pathogenic",
-    TRUE ~glvar_classification
+             by = "glvar_seqv1_description") |> 
+  mutate(glvar_icnv1_classification = case_when(
+    glvar_icnv1_description == " BRCA1 Ex02 deletion" ~"Pathogenic",
+    TRUE ~NA
   ))
 
-samples_with_gl_variants <- glvar_dnadb_classifications |> 
-  filter(!is.na(glvar_hgvs_description) |
-           !is.na(glvar_description))
+samples_with_gl_sn_variants <- glvar_dnadb_classifications |> 
+  filter(!is.na(glvar_seqv1_description))
+
+samples_with_gl_cn_variants <- glvar_dnadb_classifications |> 
+  filter(!is.na(glvar_icnv1_description))
 
 # Check all variants have a classification
-stopifnot(anyNA(samples_with_gl_variants$glvar_classification) == FALSE)
+stopifnot(anyNA(samples_with_gl_sn_variants$glvar_seqv1_classification) == FALSE)
+
+stopifnot(anyNA(samples_with_gl_cn_variants$glvar_icnv1_classification) == FALSE)
 
 # Add headline to germline results ----------------------------------------
 
@@ -72,10 +76,10 @@ glvar_dnadb_cleaned <- glvar_dnadb_classifications |>
     genotype %in% c("No pathogenic variant identified; No relevant CNV identified",
                     "No pathogenic variant identified; CNV Analysis Failed") ~"No reportable variant(s) detected",
     genotype %in% c("Fail") ~"Analysis failed (see quality score)",
-    glvar_classification %in% c("Pathogenic", "Likely pathogenic",
+    glvar_seqv1_classification %in% c("Pathogenic", "Likely pathogenic",
                                 "Uncertain significance") ~"Reportable variant(s) detected",
-    glvar_classification == "Not reported" ~"No reportable variant(s) detected",
-    !is.na(glvar_description) ~"Reportable variant(s) detected",
+    glvar_seqv1_classification == "Not reported" ~"No reportable variant(s) detected",
+    !is.na(glvar_icnv1_classification) ~"Reportable variant(s) detected",
     # Specify headline for 18028742
     (glvar_snv_result == "No pathogenic variant identified" &
       glvar_cnv_result == " No pathogenic variant identified") ~"No reportable variant(s) detected"))
@@ -93,15 +97,15 @@ tvar_classifications <- read_csv(paste0(config::get("data_folderpath"),
 
 
 tvar_dnadb_classifications <- tvar_dnadb_extracted |> 
-  mutate(tvar_hgvs_description = str_extract(string = genotype,
+  mutate(tvar_seqv1_description = str_extract(string = genotype,
                                              pattern = "(.*)\\s\\d{1,3}%",
                                              group = 1)) |> 
-  left_join(tvar_classifications, by = "tvar_hgvs_description")
+  left_join(tvar_classifications, by = "tvar_seqv1_description")
 
 samples_with_tumour_variants <- tvar_dnadb_classifications |> 
-  filter(!is.na(tvar_hgvs_description))
+  filter(!is.na(tvar_seqv1_description))
 
-stopifnot(anyNA(samples_with_tumour_variants$tvar_classification) == FALSE)
+stopifnot(anyNA(samples_with_tumour_variants$tvar_seqv1_description) == FALSE)
 
 # Add headline to tumour variant results ----------------------------------
 
@@ -116,9 +120,9 @@ tvar_dnadb_cleaned <- tvar_dnadb_classifications |>
   mutate(tvar_headline_result = case_when(
     genotype %in% no_path_var_strings ~"No reportable variant(s) detected",
     genotype %in% fail_strings ~"Analysis failed (see quality score)",
-    tvar_classification %in% c("Pathogenic", "Likely pathogenic",
+    tvar_seqv1_classification %in% c("Pathogenic", "Likely pathogenic",
                                 "Uncertain significance") ~"Reportable variant(s) detected",
-    tvar_classification == "Not reported" ~"No reportable variant(s) detected"))
+    tvar_seqv1_classification == "Not reported" ~"No reportable variant(s) detected"))
 
 stopifnot(anyNA(tvar_dnadb_cleaned$tvar_headline_result) == FALSE)
 
